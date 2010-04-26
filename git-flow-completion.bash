c5b58417ee45ea17ff16bd86d327428238392700
Justin Hileman <justin@justinhileman.info>
2010-04-26 13:06:11 -0400

    Initial commit of git-flow-completion.

---
+++ git-flow-completion.bash
@@ -0,0 +1,140 @@
+#!bash
+#
+# bash completion support for Git Flow (http://github.com/nvie/gitflow)
+#
+# Copyright (c) 2010 Justin Hileman (http://justinhileman.com)
+# Distributed under the MIT License (http://creativecommons.org/licenses/MIT/)
+#
+# The contained completion routines provide support for completing:
+#
+#  * git-flow init and version
+#  * feature, hotfix and release branches
+#  * remote feature branch names (for `git-flow feature track`)
+#
+# To use these routines:
+#
+#  0. Install git-completion
+#
+#  1. Copy this file to somewhere (e.g. ~/.git-flow-completion.sh).
+#
+#  2. Added the following line to your .bashrc:
+#      source ~/.git-flow-completion.sh
+#
+#  3. Edit git-completion.sh and add the following line to the giant $command case in _git:
+#      flow)        _git_flow ;;
+#
+# Requirement 3 will go away as soon as I figure out how to properly (and predictably) hijack
+# the `complete -F` ownership for `git` without breaking regular `git-completion`...
+
+_git_flow ()
+{
+	local subcommands="init feature release hotfix version"
+	local subcommand="$(__git_find_on_cmdline "$subcommands")"
+	if [ -z "$subcommand" ]; then
+		__gitcomp "$subcommands"
+		return
+	fi
+
+	case "$subcommand" in
+	feature)
+		__git_flow_feature
+		return
+		;;
+	release)
+		__git_flow_release
+		return
+		;;
+	hotfix)
+		__git_flow_hotfix
+		return
+		;;
+	*)
+		COMPREPLY=()
+		;;
+	esac
+}
+
+__git_flow_feature ()
+{
+	local subcommands="list start finish publish track diff rebase"
+	local subcommand="$(__git_find_on_cmdline "$subcommands")"
+	if [ -z "$subcommand" ]; then
+		__gitcomp "$subcommands"
+		return
+	fi
+
+	case "$subcommand" in
+	finish|publish|track|diff|rebase)
+		__gitcomp "$(__git_flow_list_features)"
+		return
+		;;
+	track)
+		__gitcomp "$(__git_flow_list_remote_features)"
+		return
+		;;
+	*)
+		COMPREPLY=()
+		;;
+	esac
+}
+
+__git_flow_list_features ()
+{
+	echo `git flow feature list`
+}
+
+__git_flow_list_remote_features ()
+{
+	echo `git branch -r` | grep 'origin/feature/' | awk '{ sub(/^origin\/feature\//, "", $1); print }'
+}
+
+__git_flow_release ()
+{
+	local subcommands="list start finish"
+	local subcommand="$(__git_find_on_cmdline "$subcommands")"
+	if [ -z "$subcommand" ]; then
+		__gitcomp "$subcommands"
+		return
+	fi
+	
+	case "$subcommand" in
+	finish)
+		__gitcomp "$(__git_flow_list_releases)"
+		return
+		;;
+	*)
+		COMPREPLY=()
+		;;
+	esac
+
+}
+
+__git_flow_list_releases ()
+{
+	echo `git flow release list`
+}
+
+__git_flow_hotfix ()
+{
+	local subcommands="list start finish"
+	local subcommand="$(__git_find_on_cmdline "$subcommands")"
+	if [ -z "$subcommand" ]; then
+		__gitcomp "$subcommands"
+		return
+	fi
+
+	case "$subcommand" in
+	finish)
+		__gitcomp "$(__git_flow_list_hotfixes)"
+		return
+		;;
+	*)
+		COMPREPLY=()
+		;;
+	esac
+}
+
+__git_flow_list_hotfixes ()
+{
+	echo `git flow hotfix list`
+}

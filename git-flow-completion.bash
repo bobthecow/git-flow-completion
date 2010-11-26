c24e2508c606d025c39ff31e17c9b3d6e7407b19
Justin Hileman <justin@justinhileman.info>
2010-11-26 16:03:14 -0500

    Git-flow doesn't track or publish hotfixes.

--- git-flow-completion.bash
+++ git-flow-completion.bash
@@ -150,7 +150,7 @@ __git_flow_release ()
 
 __git_flow_hotfix ()
 {
-	local subcommands="list start finish track publish help"
+	local subcommands="list start finish help"
 	local subcommand="$(__git_find_on_cmdline "$subcommands")"
 	if [ -z "$subcommand" ]; then
 		__gitcomp "$subcommands"
@@ -160,14 +160,6 @@ __git_flow_hotfix ()
 	case "$subcommand" in
 	finish)
 		__gitcomp "$(__git_flow_list_branches 'hotfix')"
-		return
-		;;
-	publish)
-		__gitcomp "$(comm -23 <(__git_flow_list_branches 'hotfix') <(__git_flow_list_remote_branches 'hotfix'))"
-		return
-		;;
-	track)
-		__gitcomp "$(__git_flow_list_remote_branches 'hotfix')"
 		return
 		;;
 	*)
